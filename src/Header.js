d8880eed7595f61aad7a69ff14a43bf87ae0e1a5
Sc0rpion2 <shazebhussaini1994@gmail.com>
2020-08-31 21:05:53 +0530

    Reactjs

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <center>
+                <h2>
+                    React app
+                </h2>
+            </center>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Header from './Header';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>Hi from Shazeb</h1>
+            <h2>NareshIT</h2>
+            <div>
+                develop by Shazeb
+            </div>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>,document.getElementById('root'))
